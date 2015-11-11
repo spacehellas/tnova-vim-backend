00cdb0decfb7877d783026d9123d6a6d663344b4
Ioannis Koutras <ioannis.koutras@gmail.com>
2015-11-12 01:23:55 +0200

    Separate database functions to a different file

--- app.js
+++ app.js
@@ -16,12 +16,10 @@
    */
 
 var winston  = require('winston');
-var influent = require('influent');
 var Hapi     = require('hapi');
 var Joi      = require('joi');
 var http     = require('http');
 var CronJob  = require('cron').CronJob;
-var Promise  = require('bluebird');
 
 // Configuration variables
 var config       = require('config');
@@ -33,116 +31,10 @@ if (typeof dbPortTemp === 'string' || dbPortTemp instanceof String) {
 } else {
   var dbPort = dbPortTemp;
 }
-var dbUsername         = config.get('database.username');
-var dbPassword         = config.get('database.password');
-var dbName             = config.get('database.name');
-var pollingInterval    = config.get('ceilometer.pollingInterval');
+var pollingInterval = config.get('ceilometer.pollingInterval');
 
 winston.level = loggingLevel;
 winston.log('info', 'T-NOVA VIM monitoring system');
-
-// Database connection instantiation
-var dbInflux = influent.createClient(
-  {
-    username   : dbUsername,
-    password   : dbPassword,
-    database   : dbName,
-    server     : [{
-      protocol : 'http',
-      host     : dbHost,
-      port     : dbPort
-    }]
-  }
-);
-
-var writeMeasurement = function(name, value, timestamp) {
-  winston.log('verbose', name + ': ' + value + ' recorded at: ' +
-    timestamp.toDate());
-  dbInflux.then(function(client) {
-    client.writeOne({
-      key: name,
-      fields: {
-        value: value
-      },
-      timestamp: timestamp.toDate()
-    });
-  });
-};
-
-var getHostLastMeasurementQuery = function(host, measurementType) {
-  var measurement = undefined;
-  var type = undefined;
-  var typeInstance = undefined;
-  var instance = undefined;
-
-  switch(measurementType) {
-    case 'cpuidle':
-      measurement = 'aggregation_value';
-      type = 'cpu';
-      typeInstance = 'idle';
-      break;
-    case 'memfree':
-      measurement = 'memory_value';
-      typeInstance = 'free';
-      break;
-    case 'fsfree':
-      measurement = 'df_value';
-      typeInstance = 'free';
-      instance = 'root'
-      break;
-  }
-
-  var query = 'SELECT last(value) FROM ' + measurement + ' WHERE host=\'' +
-    host + '\'';
-
-  if (typeof(type) !== 'undefined') {
-    query = query + ' AND type=\'' + type + '\'';
-  }
-  if (typeof(typeInstance) !== 'undefined') {
-    query = query + ' AND type_instance=\'' + typeInstance + '\'';
-  }
-  if (typeof(instance) !== 'undefined') {
-    query = query + ' AND instance=\'' + instance + '\'';
-  }
-
-  return query;
-};
-
-var readLastMeasurement = function(host, measurementType) {
-  return new Promise(function(resolve, reject) {
-    var query = getHostLastMeasurementQuery(host, measurementType);
-    winston.log('debug', 'query: ' + query);
-    dbInflux.then(function(client) {
-      client
-        .query(query)
-        .then(function(result) {
-          if ('series' in result.results[0]) {
-	    var meter = {};
-	    meter.timestamp = result.results[0].series[0].values[0][0];
-	    meter.value = result.results[0].series[0].values[0][1];
-	    switch(measurementType) {
-	      case 'cpuidle':
-		meter.units = 'jiffies';
-		break;
-	      case 'memfree':
-	        var res = formatBytes(meter.value, 2).split(' ');
-	        meter.value = res[0];
-	        meter.unit = res[1];
-		break;
-	      case 'fsfree':
-		var res = formatBytes(meter.value, 2).split(' ');
-		meter.value = res[0];
-		meter.unit = res[1];
-		break;
-	    };
-	    resolve(meter);
-          } else {
-	    reject(Error('Host or measurement type not found.'));
-	  }
-        });
-    });
-  });
-};
 
 // TODO Do not issue new token if the previous one is not expired
 
@@ -211,6 +103,8 @@ server.route({
   }
 });
 
+var db = require('./db.js');
+
 server.route({
   method: 'GET',
   path: '/api/measurements/{host}.memfree',
@@ -223,7 +117,7 @@ server.route({
       }
     },
     handler: function(request, reply) {
-      readLastMeasurement(request.params.host, 'memfree')
+      db.readLastMeasurement(request.params.host, 'memfree')
 	.then(function(result) {
 	  reply(result);
 	})
@@ -246,7 +140,7 @@ server.route({
       }
     },
     handler: function(request, reply) {
-      readLastMeasurement(request.params.host, 'cpuidle')
+      db.readLastMeasurement(request.params.host, 'cpuidle')
 	.then(function(result) {
 	  reply(result);
 	})
@@ -256,17 +150,6 @@ server.route({
     }
   }
 });
-
-function formatBytes(bytes, decimals) {
-  if (bytes == 0) {
-    return '0 Byte';
-  }
-  var k = 1000;
-  var dm = decimals + 1 || 3;
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  var i = Math.floor(Math.log(bytes) / Math.log(k));
-  return (bytes / Math.pow(k, i)).toPrecision(dm) + ' ' + sizes[i];
-}
 
 server.route({
   method: 'GET',
@@ -280,7 +163,7 @@ server.route({
       }
     },
     handler: function(request, reply) {
-      readLastMeasurement(request.params.host, 'fsfree')
+      db.readLastMeasurement(request.params.host, 'fsfree')
 	.then(function(result) {
 	  reply(result);
 	})
---
+++ db.js
@@ -0,0 +1,146 @@
+/*
+   Copyright (C) 2015  Space Hellas S.A.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
+var winston  = require('winston');
+var influent = require('influent');
+var Promise  = require('bluebird');
+var config   = require('config');
+
+var dbHost     = config.get('database.host');
+var dbPortTemp = config.get('database.port');
+if (typeof dbPortTemp === 'string' || dbPortTemp instanceof String) {
+  var dbPort   = parseInt(dbPortTemp);
+} else {
+  var dbPort   = dbPortTemp;
+}
+var dbUsername = config.get('database.username');
+var dbPassword = config.get('database.password');
+var dbName     = config.get('database.name');
+
+var dbInflux = influent.createClient({
+  username: dbUsername,
+  password: dbPassword,
+  database: dbName,
+  server: [{
+      protocol: 'http',
+      host: dbHost,
+      port: dbPort
+    }]
+});
+
+var writeMeasurement = function(name, value, timestamp) {
+  winston.log('verbose', name + ': ' + value + ' recorded at: ' +
+    timestamp.toDate());
+  dbInflux.then(function(client) {
+    client.writeOne({
+      key: name,
+      fields: {
+        value: value
+      },
+      timestamp: timestamp.toDate()
+    });
+  });
+};
+
+var getHostLastMeasurementQuery = function(host, measurementType) {
+  var measurement = undefined;
+  var type = undefined;
+  var typeInstance = undefined;
+  var instance = undefined;
+
+  switch (measurementType) {
+    case 'cpuidle':
+      measurement = 'aggregation_value';
+      type = 'cpu';
+      typeInstance = 'idle';
+      break;
+    case 'memfree':
+      measurement = 'memory_value';
+      typeInstance = 'free';
+      break;
+    case 'fsfree':
+      measurement = 'df_value';
+      typeInstance = 'free';
+      instance = 'root';
+      break;
+  }
+
+  var query = 'SELECT last(value) FROM ' + measurement + ' WHERE host=\'' +
+    host + '\'';
+
+  if (typeof(type) !== 'undefined') {
+    query = query + ' AND type=\'' + type + '\'';
+  }
+  if (typeof(typeInstance) !== 'undefined') {
+    query = query + ' AND type_instance=\'' + typeInstance + '\'';
+  }
+  if (typeof(instance) !== 'undefined') {
+    query = query + ' AND instance=\'' + instance + '\'';
+  }
+
+  return query;
+};
+
+var formatBytes = function(bytes, decimals) {
+  if (bytes == 0) {
+    return '0 Byte';
+  }
+  var k = 1000;
+  var dm = decimals + 1 || 3;
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  var i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toPrecision(dm) + ' ' + sizes[i];
+};
+
+var readLastMeasurement = function(host, measurementType) {
+  return new Promise(function(resolve, reject) {
+    var query = getHostLastMeasurementQuery(host, measurementType);
+    winston.log('debug', 'query: ' + query);
+    dbInflux.then(function(client) {
+      client
+        .query(query)
+        .then(function(result) {
+          if ('series' in result.results[0]) {
+            var meter = {};
+            meter.timestamp = result.results[0].series[0].values[0][0];
+            meter.value = result.results[0].series[0].values[0][1];
+            switch (measurementType) {
+              case 'cpuidle':
+                meter.units = 'jiffies';
+                break;
+              case 'memfree':
+                var res = formatBytes(meter.value, 2).split(' ');
+                meter.value = res[0];
+                meter.unit = res[1];
+                break;
+              case 'fsfree':
+                var res = formatBytes(meter.value, 2).split(' ');
+                meter.value = res[0];
+                meter.unit = res[1];
+                break;
+            };
+            resolve(meter);
+          } else {
+            reject(Error('Host or measurement type not found.'));
+          }
+        });
+    });
+  });
+};
+
+exports.writeMeasurement    = writeMeasurement;
+exports.readLastMeasurement = readLastMeasurement;
